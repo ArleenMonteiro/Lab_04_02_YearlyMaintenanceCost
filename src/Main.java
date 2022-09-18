88c355085347dd4bfbe65b354cc9965575eb0431
arleenmonteiro <monteiak@mail.uc.edu>
2022-09-18 19:35:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double fallMaintenance=2537;
+        double winterMaintenance=5749;
+        double springMaintenance=2238;
+        double summerMaintenance=1683;
+        double yearlyMaintenance = fallMaintenance + winterMaintenance + springMaintenance + summerMaintenance;
+        System.out.println("The fall maintenance cost is: " + fallMaintenance);
+        System.out.println("The winter maintenance cost is: " + winterMaintenance);
+        System.out.println("The spring maintenance cost is: " + springMaintenance);
+        System.out.println("The summer maintenance cost is: " + summerMaintenance);
+        System.out.println("The yearly maintenance cost is: " + yearlyMaintenance);
+
+    }
+}
